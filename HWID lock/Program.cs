d9883557a11c7405e47b0df3e6152304379ba5b9
gjjun <rlawnstjddl12@gmail.com>
2021-09-17 12:16:41 +0900

    Upload Files
    Upload Files

---
+++ HWID lock/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HWID_lock
+{
+    static class Program
+    {
+        private static string GetHWID()
+        {
+            RegistryKey registryKey;
+
+            if (Environment.Is64BitOperatingSystem)
+            {
+                registryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            }
+            else
+            {
+                registryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+            }
+
+            return registryKey.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion").GetValue("ProductId").ToString();
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            string userid = GetHWID();
+            string[] users = new string[100];
+            users[0] = "00330-53530-77725-AAOEM"; // your hardware id   0 ~ 100
+            users[1] = ""; // your hardware id
+            users[2] = ""; // your hardware id 
+            users[3] = ""; // your hardware id
+            for (int i = 0; i < users.Length; i++)
+            {
+                if (users[i] == GetHWID())
+                {
+                    MessageBox.Show("Welcome");
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("하드번호가 맞지 않습니다.");
+                    return;
+                }
+            }
+        }
+    }
+}
